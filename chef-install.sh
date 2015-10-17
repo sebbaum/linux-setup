35aa60e049c835d3506249ac8d60e6c8068abba6
Sebastian Baum <seb.baum@googlemail.com>
2015-10-17 13:50:57 +0200

    - added script to install chef

---
+++ chef-install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# install virtual box
+sudo apt-get install virtualbox virtualbox-guest-additions-iso -y
+
+# install vagrant
+sudo apt-get install vagrant -y
+
+# install chef
+#sudo apt-get install chef -y

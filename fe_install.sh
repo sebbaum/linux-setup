e46cfe7122ff02540caeb21925a0a06602bda695
Sebastian Baum <seb.baum@googlemail.com>
2016-01-20 09:05:50 +0100

    Update fe_install.sh

--- fe_install.sh
+++ fe_install.sh
@@ -4,6 +4,10 @@
 curl -sL https://deb.nodesource.com/setup_5.x | sudo -E bash -
 apt-get install nodejs -y
 
+# update npm to latest version
+# cd /usr/local/lib/node_modules/
+# npm install npm@latest
+
 # install bower
 npm install -g bower
 

5bb7cd82b2bb68f385e218c8510a6d3070d00d94
Sebastian Baum <seb.baum@googlemail.com>
2017-10-02 12:00:38 +0200

    update to latest installation process

--- kubuntu/docker-install.sh
+++ kubuntu/docker-install.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
-#http://askubuntu.com/questions/472412/how-do-i-upgrade-docker
+# https://docs.docker.com/engine/installation/linux/docker-ce/ubuntu/#install-using-the-repository
 
-sudo add-apt-repository ppa:docker-maint/testing
 sudo apt-get update
-sudo apt-get install docker.io
-docker -v
-wget -qO- https://get.docker.io/gpg | sudo apt-key add -
-sudo sh -c "echo deb http://get.docker.io/ubuntu docker main > /etc/apt/sources.list.d/docker.list"
-sudo apt-get update
-sudo apt-get install lxc-docker
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+sudo apt-get install docker-ce

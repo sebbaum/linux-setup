cc7b087f0bb3a69d2a5cff397f39e531132c2a93
Sebastian Baum <seb.baum@googlemail.com>
2017-12-16 13:17:52 +0100

    Added fix for npm permissions

--- fe_install.sh
+++ fe_install.sh
@@ -4,6 +4,12 @@
 curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
+mkdir ~/.npm-global
+npm config set prefix '~/.npm-global'
+#Open or create a ~/.profile file and add this line:
+export PATH=~/.npm-global/bin:$PATH
+source ~/.profile
+
 # update npm to latest version
 # cd /usr/local/lib/node_modules/
 # npm install npm@latest

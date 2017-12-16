f4031b04180868cc84b1bb49dd3d017c843152fe
Sebastian Baum <seb.baum@googlemail.com>
2017-12-16 13:07:57 +0100

    Upgrade nodejs to version 8

--- fe_install.sh
+++ fe_install.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 # install nodejs
-curl -sL https://deb.nodesource.com/setup_6.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 # update npm to latest version
 # cd /usr/local/lib/node_modules/
 # npm install npm@latest
-
-# install bower
-npm install -g bower
 
 # install gulp
 npm install --global gulp

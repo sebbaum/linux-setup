b3acf57b42e4af5d64b29e30c123ea2e0e43cd9e
Sebastian Baum <seb.baum@googlemail.com>
2018-01-28 11:14:16 +0100

    Install script for php7.1

---
+++ php7-1.sh
@@ -0,0 +1,18 @@
+#!/etc/bash
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+
+sudo apt install php7.1 \
+php7.1-common \
+php7.1-fpm \
+php7.1-json \
+php7.1-mysql \
+php7.1-readline \
+php7.1-xml \
+php7.1-cli \
+php7.1-curl \
+php7.1-gd \
+php7.1-mbstring \
+php7.1-opcache \
+php7.1-sqlite3 \
+php7.1-zip
